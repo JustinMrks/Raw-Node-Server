97db729d11dc31fdb455713709bca6813db99328
justinmrks <justinmarks19@gmail.com>
2022-09-20 00:15:31 -0500

    Working with breakout files

--- app.js
+++ app.js
@@ -1,43 +1,8 @@
 const http = require('http');
-const fs = require('fs');
-const { PassThrough } = require('stream');
+const routes = require('./routes');
 
-const PORT = 3000;
+const PORT = routes.port;
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head> <h1>Enter Message</h1></head>');
-    res.write(
-      '<body><form action="/message" method="POST"> <input type="text" name="message"><button type="submit">Send</button></input></form></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log('Logging', chunk);
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head> <h1>File Updated!</h1></head>');
-  res.write('</html>');
-  return res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(PORT);
