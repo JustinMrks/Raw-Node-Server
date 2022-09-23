dd185afe3179f2afbf4d2f9998ac33cfc331c875
justinmrks <justinmarks19@gmail.com>
2022-09-23 13:47:55 -0500

    Basic Express routing

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const http = require('http');
-
 const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(3000);
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+
+app.listen(3000);
